15d8676833a9db13370862f3007561d7a2858d80
Dhaqane-00 <Abdilaahimowliid@gmail.com>
2025-01-17 17:55:10 +0300

    Enhance Pricing component: replace static content with a styled layout featuring a background image and centered title for improved visual presentation.

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import PageIllustration from './page-illustration'
+import bgPricing from '../public/images/Hero2.svg'
 
 function Pricing() {
   return (
-    <div>Pricing</div>
+    <div className='h-screen bg-[url("/images/bgPricing.svg")] bg-cover bg-center bg-no-repeat'>
+        <div className='flex flex-col items-center justify-center h-full'>
+            <h1 className='text-4xl font-bold '>Pricing</h1>
+        </div>
+    </div>
   )
 }
 
